f2a1164617313c933e583caa8c8d5393cb4d6581
dawit-getahun-mengistu <dawitgetahunmengistu@gmail.com>
2025-06-18 22:11:51 +0300

    fix listening

--- apps/user-be/src/main.ts
+++ apps/user-be/src/main.ts
@@ -43,6 +43,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
